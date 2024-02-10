40eccc977d8bc683b79f96fbfade34f4ad9f1053
tim <tim@timschupp.de>
2024-02-10 16:41:56 +0100

    what?

--- backend/chat/api/chats.py
+++ backend/chat/api/chats.py
@@ -3,6 +3,11 @@ from rest_framework import response
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from chat.models import Chat, ChatSerializer, ChatInModelSerializer
 from chat.api.viewsets import UserStaffRestricedModelViewsetMixin, DetailedPaginationMixin
+from drf_spectacular.utils import inline_serializer
+
+from rest_framework import serializers
+from drf_spectacular.utils import extend_schema
+
 
 class ChatsModelViewSet(viewsets.ModelViewSet, UserStaffRestricedModelViewsetMixin):
     """
@@ -14,6 +19,25 @@ class ChatsModelViewSet(viewsets.ModelViewSet, UserStaffRestricedModelViewsetMix
     permission_classes = [IsAuthenticated]
     pagination_class = DetailedPaginationMixin
     queryset = Chat.objects.all().order_by("-created")
+    
+    @extend_schema(responses={
+        200: inline_serializer(
+            name='PaginatedResponse',
+            fields={
+                'count': serializers.IntegerField(),
+                'next': serializers.URLField(),
+                'previous': serializers.URLField(),
+                'results': ChatSerializer(many=True),
+                'page_size': serializers.IntegerField(),
+                'next_page': serializers.IntegerField(allow_null=True),
+                'previous_page': serializers.IntegerField(allow_null=True),
+                'last_page': serializers.IntegerField(),
+                'first_page': serializers.IntegerField(),
+            }
+        )
+    })
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def get_queryset(self):
         if not self.request.user.is_staff:
