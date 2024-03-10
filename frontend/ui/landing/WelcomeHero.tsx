786f925d2492ec66841efaf2811d3a40d8e2bc4e
JannisToelle <github.z8zet@simplelogin.com>
2024-03-10 12:56:28 +0100

    Example on WelcomeHero

--- frontend/ui/landing/WelcomeHero.tsx
+++ frontend/ui/landing/WelcomeHero.tsx
@@ -1,8 +1,11 @@
 export default WecomeHero;
 
-import React from "react";
+import { func } from "prop-types";
 import ThemeSelector from "../atoms/ThemeSelector";
-import UserIndicator from "../atoms/UserIndicator";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
 
 function FeaetureIcon({ title, children }) {
   return (
@@ -21,10 +24,133 @@ function FeaetureIcon({ title, children }) {
 
 function WecomeHero() {
   const pageSource = typeof window !== "undefined" ? window.location.href : "";
+  const components: { title: string; href: string; description: string }[] = [
+    {
+      title: "Alert Dialog",
+      href: "/docs/primitives/alert-dialog",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Hover Card",
+      href: "/docs/primitives/hover-card",
+      description:
+        "For sighted users to preview content available behind a link.",
+    },
+    {
+      title: "Progress",
+      href: "/docs/primitives/progress",
+      description:
+        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    },
+    {
+      title: "Scroll-area",
+      href: "/docs/primitives/scroll-area",
+      description: "Visually or semantically separates content.",
+    },
+    {
+      title: "Tabs",
+      href: "/docs/primitives/tabs",
+      description:
+        "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+    },
+    {
+      title: "Tooltip",
+      href: "/docs/primitives/tooltip",
+      description:
+        "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+    },
+  ];
+
   return (
     <div className="h-screen w-screen relative bg-content">
-      <div className="w-full h-full flex p-10 content-center items-center justify-center relative">
-        <div className="w-full h-full flex flex-col gap-6 content-center items-center justify-center relative">
+      <div className="navbar bg-base-100 w-full p-10">
+        <div className="flex-1">
+          <div className="w-10 rounded-full">
+            <img alt="Tailwind CSS Navbar component" src="assets/logo.png" />
+          </div>
+          <a className="btn btn-ghost text-xl" href="https://msgmate.io">
+            msgmate.io
+          </a>
+          <button className="btn">Test</button>
+        </div>
+        <div className="flex-none">
+          <a role="button" className="btn btn-ghost" href="login">
+            Login
+          </a>
+          <a role="button" className="btn btn-ghost" href="register">
+            Register
+          </a>
+
+          <ThemeSelector />
+        </div>
+      </div>
+
+      <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      shadcn/ui
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components that you can copy and
+                      paste into your apps. Accessible. Customizable. Open
+                      Source.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Button variant="link">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Button>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+
+      <div className="w-full h-full flex p-10 relative">
+        {/* <div className="w-full h-full flex flex-col gap-6 content-center items-center justify-center relative"></div> */}
+
+        {/* <div className="w-full h-full flex flex-col gap-6 content-center items-center justify-center relative">
           <div className="border-2 rounded-2xl">
             <h1 className="text-2xl sm:text-4xl lg:text-7xl">
               {" "}
@@ -66,8 +192,34 @@ function WecomeHero() {
               <UserIndicator />
             </FeaetureIcon>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
