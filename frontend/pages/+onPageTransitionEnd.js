d3c51c96929f7d12a20280175e063a435b54e364
Tim Schupp <tim@timschupp.de>
2024-03-14 00:06:40 +0100

    updated packages removed old page transitions

--- frontend/pages/+onPageTransitionEnd.js
+++ frontend/pages/+onPageTransitionEnd.js
@@ -1,12 +1,4 @@
 export { onPageTransitionEnd };
 
 async function onPageTransitionEnd(pageContext) {
-  console.log("Page transition end");
-  console.log("Is backwards navigation?", pageContext.isBackwardNavigation);
-  document
-    .getElementById("transitionContainer")
-    .classList.remove("page-is-transitioning");
-  document
-    .getElementById("chatView")
-    ?.classList.remove("page-is-transitioning");
 }
--- frontend/pages/+onPageTransitionStart.js
+++ frontend/pages/+onPageTransitionStart.js
@@ -1,10 +1,4 @@
 export { onPageTransitionStart };
 
 async function onPageTransitionStart(pageContext) {
-  console.log("Page transition start");
-  console.log("Is backwards navigation?", pageContext.isBackwardNavigation);
-  document
-    .getElementById("transitionContainer")
-    .classList.add("page-is-transitioning");
-  document.getElementById("chatView")?.classList.add("page-is-transitioning");
 }
