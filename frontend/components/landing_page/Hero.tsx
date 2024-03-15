1d1d7ec15e813c622da55ea076b0d83c13a804eb
Tim Schupp <tim@timschupp.de>
2024-03-16 00:40:26 +0100

    inital imports adoption and fork / clone of https://github.com/leoMirandaa/shadcn-landing-page

---
+++ frontend/components/landing_page/Hero.tsx
@@ -0,0 +1,46 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { HeroCards } from "./HeroCards";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+export const Hero = ({
+  cinematicTitle = (<></>),
+  subtitle = "subtitle",
+  githubLink = ""
+}) => {
+  return (
+    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
+      <div className="text-center lg:text-start space-y-6">
+        <main className="text-5xl md:text-6xl font-bold">
+          {cinematicTitle}
+        </main>
+
+        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+          {subtitle}
+        </p>
+
+        <div className="space-y-4 md:space-y-0 md:space-x-4">
+          <Button className="w-full md:w-1/3">Get Started</Button>
+
+          <a
+            href={githubLink}
+            target="_blank"
+            className={`w-full md:w-1/3 ${buttonVariants({
+              variant: "outline",
+            })}`}
+          >
+            Github Repository
+            <GitHubLogoIcon className="ml-2 w-5 h-5" />
+          </a>
+        </div>
+      </div>
+
+      {/* Hero cards sections */}
+      <div className="z-10">
+        <HeroCards />
+      </div>
+
+      {/* Shadow effect */}
+      <div className="shadow"></div>
+    </section>
+  );
+};
---
+++ frontend/components/landing_page/README.md
@@ -0,0 +1,5 @@
+# Project Landing Page
+
+This is a fork / port of: https://github.com/leoMirandaa/shadcn-landing-page/blob/main/src/App.tsx
+
+Adapted to work with vike.dev & dasyui color shemes / themes.
