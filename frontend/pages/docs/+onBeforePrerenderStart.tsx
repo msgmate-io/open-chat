d655cc57c722a92c188fcb4f36eadcf311143e68
Tim Schupp <tim@timschupp.de>
2024-03-20 15:59:34 +0100

    on before renderer paths must start with /

--- frontend/pages/docs/+onBeforePrerenderStart.tsx
+++ frontend/pages/docs/+onBeforePrerenderStart.tsx
@@ -7,6 +7,6 @@ import { docsConfig } from './config'
 const onBeforePrerenderStart: OnBeforePrerenderStartAsync = async (
 ): ReturnType<OnBeforePrerenderStartAsync> => {
     return docsConfig.sidebarNav[0].items.map((item) => {
-        return `docs/${item.href}`
+        return `/docs${item.href}`
     })
 }
