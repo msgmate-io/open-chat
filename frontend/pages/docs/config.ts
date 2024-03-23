7c227d729cb8ed188a3d3998687afe68feab8081
Tim Schupp <tim@timschupp.de>
2024-03-23 01:08:15 +0100

    (wip) docs on k8s helm deployment

--- frontend/pages/docs/config.ts
+++ frontend/pages/docs/config.ts
@@ -1,6 +1,8 @@
 import { lazy } from "react";
 import Intro from "./pages/Intro.mdx";
 import Development from "./pages/Development.mdx";
+import CapacitorDevelopment from "./pages/CapacitorDevelopment.mdx";
+import GithubActions from "./pages/GithubActions.mdx";
 import HelmDeployment from "./pages/HelmDeployment.mdx";
 
 
@@ -21,13 +23,18 @@ export const docsConfig = {
                     items: [],
                 },
                 {
-                    title: "Local Installation",
-                    href: "/dev-local",
+                    title: "Capacitor Native App",
+                    href: "/dev-capacitor",
                     items: [],
                 },
                 {
                     title: "Helm deployment",
                     href: "/helm-deployment",
+                    items: [],
+                },
+                {
+                    title: "Github Actions",
+                    href: "/github-actions",
                     items: [],
                 },
             ],
@@ -45,7 +52,15 @@ export const pages = [
         component: Development
     },
     {
+        route: "dev-capacitor",
+        component: CapacitorDevelopment
+    },
+    {
         route: "helm-deployment",
         component: HelmDeployment
-    }
+    },
+    {
+        route: "github-actions",
+        components: GithubActions
+    },
 ]
