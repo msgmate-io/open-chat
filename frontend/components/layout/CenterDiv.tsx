c1a086a2edc6b4149782ebcfda262bc425d454bc
Tim Schupp <tim@timschupp.de>
2024-05-05 17:47:01 +0200

    move components to opensource sub-repo, so they can be reused and we can bundle a includable script package that can render the chat interface on any app

--- frontend/components/layout/CenterDiv.tsx
+++
@@ -1,7 +0,0 @@
-export default function CenterDiv({ children, className }: { children: React.ReactNode, className?: string }) {
-    return (
-        <div className={`flex justify-center content-center items-center w-screen ${className}`}>
-            {children}
-        </div>
-    );
-}
