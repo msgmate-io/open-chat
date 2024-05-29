15ec37f8b7a9a76dd655f5fa8a641b363978fc0e
Tim Schupp <tim@timschupp.de>
2024-05-29 20:24:22 +0200

    remove open chat docs from new dev repo ( they are open-chat specific )

--- frontend/pages/docs/+Page.tsx
+++
@@ -1,9 +0,0 @@
-import IntroContent from "./pages/Intro.mdx";
-import { DocsLayout } from "./DocsLayout";
-
-
-export function Page() {
-    return <DocsLayout pathname={""}>
-        <IntroContent />
-    </DocsLayout>
-}
