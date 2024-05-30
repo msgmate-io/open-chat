4b414e87188b210374b38ce32aeba7e80ec6e61f
Tim Schupp <tim@timschupp.de>
2024-05-30 23:08:42 +0200

    reset connection on init: TODO should be a beat shedule, that pings clients instead

--- backend/core/tools.py
+++ backend/core/tools.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from chat.models import ChatConnections
+from django.db.models import Q
 
 def base_admin_exists():
     return get_user_model().objects.filter(username=settings.BASE_ADMIN_USERNAME).exists()
@@ -15,6 +17,14 @@ def get_or_create_base_admin():
         user = user.first()
     return user
 
+def reset_all_chat_connections():
+    ChatConnections.objects.filter(
+        Q(is_online=True) | Q(connection_counter__gt=0) | Q(connection_counter__lt=0)
+    ).update(
+        is_online=False,
+        connection_counter=0
+    )
+
 def before_backend_startup():
     from core.default_user_setup import create_or_reset_test_users, create_or_reset_admin_user, create_or_reset_base_bot_users
     from core.setup_base_permissions import setup_base_permissions
@@ -27,3 +37,6 @@ def before_backend_startup():
     
     # 3 - Create default bot users ( atm only hal9003 )
     create_or_reset_base_bot_users()
+    
+    # 4 - Reset all chat connections
+    reset_all_chat_connections()
