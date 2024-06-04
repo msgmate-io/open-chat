5dc1e55cbc7e84b8990f6543720aabbd0f36b692
Tim Schupp <tim@timschupp.de>
2024-06-05 01:02:23 +0200

    remove old imports

--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from django.urls import path, include, re_path
-from django.http import HttpResponse
-from django_nextjs.render import render_nextjs_page_sync, render_nextjs_page_async
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django_nextjs.proxy import NextJSProxyView
-from django_nextjs import urls
 
 urlpatterns = [
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
