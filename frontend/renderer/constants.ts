fb67f6bd7614ed4acb15930e046c54794fef0862
Tim Schupp <tim@timschupp.de>
2024-06-08 23:14:38 +0200

    updated components

--- frontend/renderer/constants.ts
+++ frontend/renderer/constants.ts
@@ -1,2 +1,3 @@
 export const LOGIN_ROUTE = import.meta.env.PUBLIC_ENV__LOGIN_ROUTE || "/login";
 export const ROUTE_PREFIX = import.meta.env.PUBLIC_ENV__ROUTE_PREFIX || "";
+export const DEBUG = import.meta.env.PUBLIC_ENV__DEBUG || false;
