6e10ad6c1771add54904ad96fff7901db520b162
Tim Schupp <tim@timschupp.de>
2024-08-20 23:39:35 +0200

    minor updates for electron client usage

--- frontend/pages/chat/+guard.ts
+++ frontend/pages/chat/+guard.ts
@@ -6,8 +6,8 @@ import type { GuardAsync } from 'vike/types'
 import { IS_NATIVE } from '../../renderer/constants'
 
 const guard: GuardAsync = async (pk): ReturnType<GuardAsync> => {
-    console.log("GUARD", pk, IS_NATIVE)
     if (IS_NATIVE) {
+        // check if electron
         return
     }
     if (typeof window !== undefined && Cookies.get("clientAuthorized")) {
--- frontend/pages/index/+Page.tsx
+++ frontend/pages/index/+Page.tsx
@@ -1,9 +1,23 @@
 export { Page };
-import { LandingForwardHero, LandingHero } from "@open-chat/open-chat-ui";
-import { DOCS_AT_INDEX } from "../../renderer/constants";
+import { GlobalContext, LandingForwardHero, LandingHero, useIsNativeClientAuthorized } from "@open-chat/open-chat-ui";
+import { useContext, useEffect, useState } from "react";
+import { DOCS_AT_INDEX, IS_NATIVE } from "../../renderer/constants";
 
 function Page() {
   const hero = DOCS_AT_INDEX ? "forward" : "default";
+  const isClientAuthorized = useIsNativeClientAuthorized();
+  const { navigate } = useContext(GlobalContext);
+  const [startupRedirected, setStartupRedirected] = useState(false);
+
+  useEffect(() => {
+    if (IS_NATIVE) {
+      console.log("IS_NATIVE isClientAuthorized", isClientAuthorized)
+      if (isClientAuthorized && !startupRedirected) {
+        setStartupRedirected(true);
+        navigate("/chat")
+      }
+    }
+  }, [isClientAuthorized]);
 
   return hero == "forward" ? <LandingForwardHero /> : <LandingHero />
 }
