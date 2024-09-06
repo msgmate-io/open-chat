593306c81d7c15bb06530dc263966aba9836a0cf
Tim Schupp <tim@timschupp.de>
2024-09-07 01:35:03 +0200

    auto-reload celery workers in development

--- hal/spinup.sh
+++ hal/spinup.sh
@@ -6,8 +6,14 @@ if [ $CREATE_TEST_USERS = "true" ]; then
     python3 manage.py shell --command 'from core.default_user_setup import create_or_reset_test_users; create_or_reset_test_users(4)'
 fi
 
-celery -A conf worker --loglevel=info &
-SINGLE_BEAT_REDIS_SERVER="$REDIS_URL" single-beat celery -A conf beat --loglevel=info &
+if [ $DJANGO_DEBUG = "true" ]; then
+    # In debug mode we auto-reload the workers
+    watchmedo auto-restart -d . -p '*.py' -- celery -A conf worker --loglevel=info &
+    SINGLE_BEAT_REDIS_SERVER="$REDIS_URL" single-beat celery -A conf beat --loglevel=info &
+else
+    celery -A conf worker --loglevel=info &
+    SINGLE_BEAT_REDIS_SERVER="$REDIS_URL" single-beat celery -A conf beat --loglevel=info &
+fi
 
 function start_flower() {
     sleep 5
@@ -16,5 +22,6 @@ function start_flower() {
 
 
 start_flower &
+
 
 uvicorn conf.asgi:application --reload --port 8000 --host 0.0.0.0
